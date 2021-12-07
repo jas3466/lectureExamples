faaa2d4175b67a7c7cc4f2b82c0521b4e1d87c2d
julie <julie@DESKTOP-5L3T3C6>
2021-12-07 10:59:22 -0600

    Added Tuesday, December 7

---
+++ src/progLogic/AssertionTest.java
@@ -0,0 +1,69 @@
+package progLogic;
+
+import java.util.*;
+
+public class AssertionTest {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int one = Integer.parseInt(s.nextLine());
+        System.out.print("Enter second number: ");
+        int two = Integer.parseInt(s.nextLine());
+
+        int answer = divide(one, two);
+        assert(answer == one/two);
+
+        int[] nums = {1,2,3,4};
+        swap(nums, 0, 4);
+
+        System.out.println("\nSwapped:");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+
+    /**
+     * Returns the integer division of two whole numbers
+     *
+     * @param a - the first number
+     * @param b - the second number
+     *            requires b != 0
+     * @return a/b, using integer division
+     */
+    public static int divide(int a, int b) {
+        assert(b != 0);
+
+        return a/b;
+    }
+
+    /**
+     * Swaps two elements in an array
+     *
+     * <h4>Precondition:</h4>
+     * {@code
+     * 0 <= pos1 < arr.length and 0 <= pos2 < arr.length
+     * }
+     *
+     * <h4>Postcondition:</h4>
+     *      <pre>
+     *      {@code
+     *      arr[pos1] == arr_in[pos2]
+     *      arr[pos2] == arr_in[pos1]
+     *      For all positions i, if i != pos1 and i != pos2,
+     *          then arr[i] == arr_in[i]
+     *      }
+     *      </pre>
+     *
+     * @param arr - an integer array
+     * @param pos1 - the first position to swap
+     * @param pos2 - the second position to swap
+     */
+    public static void swap(int[] arr, int pos1, int pos2) {
+        assert(pos1 >= 0 && pos1 < arr.length);
+        assert(pos2 >= 0 && pos2 < arr.length);
+
+        int temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
+    }
+}
